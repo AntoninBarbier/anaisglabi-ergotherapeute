c27b0b609c229195a8671c35b83fb34dd0414a0c
AntoninBarbier <skery.antonin@outlook.com>
2024-03-17 16:16:18 +0100

    try to fix build import error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Navigation from "./components/Navigation";
 import Header from "./components/Sections/Header";
 import ErgotherapySection from "./components/Sections/Ergotherapy";
 import ConsultationSection from "./components/Sections/Consultation";
-import AboutSection from "./components/Sections/About";
-import AppointmentSection from "./components/Sections/Appointment";
-import FundingSection from "./components/Sections/Funding";
-import Footer from "./components/Sections/Footer";
+import AboutSection from "./components/Sections/About/index";
+import AppointmentSection from "./components/Sections/Appointment/index";
+import FundingSection from "./components/Sections/Funding/index";
+import Footer from "./components/Sections/Footer/index";
 
 export default function Home() {
 	return (
