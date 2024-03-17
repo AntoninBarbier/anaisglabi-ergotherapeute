1c2d3119d256cc3193fdfbe7c02e589d1f59a146
AntoninBarbier <skery.antonin@outlook.com>
2024-03-17 16:20:18 +0100

    Try fix netlify build errors

--- src/app/components/sections/Header/index.tsx
+++ src/app/components/sections/Header/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRef } from "react";
-import styles from "./styles.module.css";
+import styles from "./styles.module.scss";
 import Image from "next/image";
 import Button from "../../UI/Button";
 
